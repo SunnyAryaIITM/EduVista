05c72ecbb27b29da2913f5e39aa091eca2bca698
SunnyAryaIITM <22f2001129@ds.study.iitm.ac.in>
2023-12-25 16:19:36 +0530

    Configuration Done

---
+++ application/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_caching import Cache
+from flask_restful import Api
+
+app = None
+
+def create_admin():
+    pass
+
+def create_app():
+    pass
+
+
+from application.resources import *
+from application.routes import *
---
+++ application/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.sqlite3'
+    SQLALCHEMY_MODIFICATIONS = False
+    SECRET_KEY = 'vykdgtlghastd)jkdfj77lkfew&fksdj)lf(dkljf)kd'
